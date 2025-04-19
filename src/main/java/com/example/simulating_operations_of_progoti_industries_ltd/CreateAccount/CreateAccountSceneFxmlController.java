de5196ecaa7356de5082d8fc496f0bfc92339684
SaimaSinthiya <157502331+SaimaSinthiya@users.noreply.github.com>
2025-04-19 11:44:09 +0600

    test changes1

---
+++ src/main/java/com/example/simulating_operations_of_progoti_industries_ltd/CreateAccount/CreateAccountSceneFxmlController.java
@@ -0,0 +1,39 @@
+package com.example.simulating_operations_of_progoti_industries_ltd.CreateAccount;
+
+public class CreateAccountSceneFxmlController
+{
+    @javafx.fxml.FXML
+    private TextField phoneNoTextField;
+    @javafx.fxml.FXML
+    private Label showOutput1Label;
+    @javafx.fxml.FXML
+    private PasswordField pwPasswordField;
+    @javafx.fxml.FXML
+    private TextField yyyyTextField;
+    @javafx.fxml.FXML
+    private TextField emailTextField;
+    @javafx.fxml.FXML
+    private TextField nameTextField;
+    @javafx.fxml.FXML
+    private PasswordField cpwPasswordField;
+    @javafx.fxml.FXML
+    private ComboBox userTypeComboBox;
+    @javafx.fxml.FXML
+    private RadioButton femaleRadioButton;
+    @javafx.fxml.FXML
+    private TextField ddTextField;
+    @javafx.fxml.FXML
+    private TextField mmTextField;
+    @javafx.fxml.FXML
+    private Label showOutput2Label;
+    @javafx.fxml.FXML
+    private RadioButton maleRadioButton;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void createOnMouseClick(Event event) {
+    }
+}
